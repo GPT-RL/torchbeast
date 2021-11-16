438b335e7a145ce7c44050fa035439575572487d
Ethan Brooks <ethanabrooks@gmail.com>
2021-11-16 11:04:39 -0500

    get to pass pre-commit

--- tests/lint_changed.sh
+++ tests/lint_changed.sh
@@ -11,29 +11,29 @@ set -e
 
 CMD="flake8"
 CHANGED_FILES="$(git diff --diff-filter=d --name-only main... | grep '\.py$' | grep -v "torchbeast/atari_wrappers.py" | tr '\n' ' ')"
-while getopts bi opt; do
-  case $opt in
-    b)
-      CMD="black"
-  esac
+while getopts b opt; do
+	case $opt in
+	b)
+		CMD="black"
+		;;
+	*) echo "usage" ;;
+	esac
 
-  done
+done
 
-if [ "$CHANGED_FILES" != "" ]
-then
-    if [[ "$CMD" == "black" ]]
-    then
-        command -v black >/dev/null || \
-            ( echo "Please install black." && false )
-        # Only output if something needs to change.
-        black --check $CHANGED_FILES
-    else
-        flake8 --version | grep '^3\.[6-9]\.' >/dev/null || \
-            ( echo "Please install flake8 >=3.6.0." && false )
+if [ "$CHANGED_FILES" != "" ]; then
+	if [ "$CMD" = "black" ]; then
+		command -v black >/dev/null ||
+			(echo "Please install black." && false)
+		# Only output if something needs to change.
+		black --check "$CHANGED_FILES"
+	else
+		flake8 --version | grep '^3\.[6-9]\.' >/dev/null ||
+			(echo "Please install flake8 >=3.6.0." && false)
 
-        # Soft complaint on too-long-lines.
-        flake8 --select=E501 --show-source $CHANGED_FILES
-        # Hard complaint on really long lines.
-        exec flake8 --max-line-length=127 --show-source $CHANGED_FILES
-    fi
+		# Soft complaint on too-long-lines.
+		flake8 --select=E501 --show-source "$CHANGED_FILES"
+		# Hard complaint on really long lines.
+		exec flake8 --max-line-length=127 --show-source "$CHANGED_FILES"
+	fi
 fi
